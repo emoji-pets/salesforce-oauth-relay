19aff691aaf2ceb67603b7e39a7f187f062eec2f
Nick Bennett <nick@nickbenn.com>
2018-08-03 09:05:41 -0600

    More simplification (Spring Boot-ification).

--- src/main/java/edu/cnm/deepdive/salesforceoauthrelay/converter/BaseCredentialFormConverter.java
+++ src/main/java/edu/cnm/deepdive/salesforceoauthrelay/converter/BaseCredentialFormConverter.java
@@ -14,9 +14,11 @@ import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+@Component
 public class BaseCredentialFormConverter extends AbstractHttpMessageConverter<BaseCredential> {
 
   private static final FormHttpMessageConverter converter = new FormHttpMessageConverter();
